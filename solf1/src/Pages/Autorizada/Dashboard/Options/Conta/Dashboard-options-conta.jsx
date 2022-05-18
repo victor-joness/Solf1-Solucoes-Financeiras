44043bc204cb777ed752447e3180e7ac529fb129
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-05-18 09:44:00 -0300

    Create Dashboard-options-conta.jsx

---
+++ solf1/src/Pages/Autorizada/Dashboard/Options/Conta/Dashboard-options-conta.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Dashboard-options-conta.css";
+
+const PageDashboardOptionsConta = () => {
+    return (
+        <div><h1>teste de conta</h1></div>
+    )
+}
+
+export default PageDashboardOptionsConta;
