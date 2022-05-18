e21a713573bf39a01242dc43f0b2363e4fcf2096
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-05-18 09:43:48 -0300

    Create Dashboard.jsx

---
+++ solf1/src/Pages/Autorizada/Dashboard/Dashboard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./Dashboard.css";
+
+import UserContainer from "../../../Components/User/UserContainer";
+import OptionsContainer from "../../../Components/Options/OptionsContainer";
+import Menulateral from "../../../Components/NavBar/menulateral";
+import avatar from "../../../images/AVATAR-ICON.png";
+
+
+const PageAutorizadaDashboard = () => {
+    return (
+        <div className="Dashboard--Container">
+            <Menulateral></Menulateral>
+
+            <div className="Dashboard--Container--direita">
+                <div className="Dashboard--Container--direita__usuario">
+                    <img src={avatar} alt="avatar do usuario" />
+                    <h1>Nome do Usuário</h1>
+                </div>
+
+                <div className="Dashboard--Container--direita__menu">
+                    <UserContainer></UserContainer>
+                    <OptionsContainer></OptionsContainer>
+                </div>
+
+                <div className="Dashboard--Container--direita__footer">
+                    <p>Criado por <span>Victor Mesquita</span> & <span>Livian</span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PageAutorizadaDashboard;
