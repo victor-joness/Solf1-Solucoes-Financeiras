91af42f4cc0647884f65acadf727203d5cb63ab1
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-05-19 08:02:08 -0300

    Create signin.jsx

---
+++ solf1/src/Pages/Autorizada/Login/SignIn/signin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./signin.css";
+
+const PageLoginSignIn = () => {
+    return(
+        <div><h1>escolheu sign in</h1></div>
+    )
+}
+
+export default PageLoginSignIn;
