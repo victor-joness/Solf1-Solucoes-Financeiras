9793adc867c40c185523e31cf89c043a26e9633d
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-05-23 09:03:11 -0300

    Update menulateralNoUser.jsx

--- Client/src/Components/NavBar No user/menulateralNoUser.jsx
+++ Client/src/Components/NavBar No user/menulateralNoUser.jsx
@@ -1,5 +1,5 @@
-import React ,{ useState }from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from 'react-router-dom';
 import logo from "../../images/LOGO.png";
 //import css
 import "./menulateral.css";
