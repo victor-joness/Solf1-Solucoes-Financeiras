56701421d7731ec39bfd9c9098bba2038b7a5020
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-05-30 09:31:16 -0300

    Update menulateralNoUser.jsx

--- Client/src/Components/NavBar No user/menulateralNoUser.jsx
+++ Client/src/Components/NavBar No user/menulateralNoUser.jsx
@@ -9,55 +9,71 @@ import user from "../../images/icons navbar/USER.png";
 import galeria from "../../images/icons navbar/GALERIA.png";
 import cartao from "../../images/icons navbar/CARTAO.png";
 import identidade from "../../images/icons navbar/IDENTIDADE.png";
+import avatar from "../../images/AVATAR-ICON.png"; 
+import exit from "../../images/EXIT.png";
 
 const MenulateralNoUser = () => {
     //pegar alguma coisa para verificar se ta logado;
     //ex
-    let logado = 0;
-
+    let logado = global.variavel;
 
     const navigate = useNavigate();
     //se nao tiver logado;
     const NaoUser = () => {
         navigate(`/login`);
-        console.log(logado)
+        console.log(logado);
+        console.log(global.variavel)
     }
 
     const NaoDashboard = () => {
         navigate(`/Naodashboard`);
+        console.log(global.variavel)
     }
 
     const Naocartões = () => {
         navigate(`/Naocartoes`);
+        console.log(global.variavel)
     }
 
     const Naoadmin = () => {
         navigate(`/Naoadmin`);
+        console.log(global.variavel)
     }
 
     //se ja tiver logado;
     const House = () => {
         navigate(`/`);
+        console.log(global.variavel)
     }
 
     const User = () => {
-        navigate(`/login`);
+        navigate(`/Logout`);
+        console.log(global.variavel)
     }
 
     const Dashboard = () => {
         navigate(`/dashboard`);
+        console.log(global.variavel)
     }
 
     const cartões = () => {
-        navigate(`/cartões`);
+        navigate(`/cartoes`);
+        console.log(global.variavel)
     }
 
     const admin = () => {
         navigate(`/admin`);
+        console.log(global.variavel)
+    }
+
+    const logout = () => {
+        global.variavel = 0;
+        window.alert("usuario deslogado com sucesso");
+        navigate(`/`);
     }
 
 
-    if(logado == 1){
+    if(logado === 1){
         return(
             <div className="container">
             <div className="container-image">
@@ -81,9 +97,79 @@ const MenulateralNoUser = () => {
                     <img src={cartao} alt="icon cartao navbar" />
                 </div>
 
+                <div className="container-icons__quinto" onClick={Naoadmin}>
+                    <img src={identidade} alt="icon identidade navbar" />
+                </div>
+            </div>
+
+            <div className="container-user">
+                <div className="container-avatar"><img src={avatar} alt="icon avatar navbar" /></div>
+                <div className="container-exit" onClick={logout}><img src={exit} alt="icon exit navbar" /></div>
+            </div>
+        </div>
+        )
+    }else if(logado === 0){
+        return (
+            <div className="container">
+            <div className="container-image">
+                <img src={logo} alt="logo do solf1"/>
+            </div>
+
+            <div className="container-icons">
+                <div className="container-icons__primeiro" onClick={House}>
+                    <img src={house} alt="icon house navbar" />
+                </div>
+
+                <div className="container-icons__segundo" onClick={NaoUser}>
+                    <img src={user} alt="icon user navbar" />
+                </div>
+
+                <div className="container-icons__terceiro" onClick={NaoDashboard}>
+                    <img src={galeria} alt="icon galeria navbar" />
+                </div>
+
+                <div className="container-icons__quarto" onClick={Naocartões}>
+                    <img src={cartao} alt="icon cartao navbar" />
+                </div>
+
+                <div className="container-icons__quinto" onClick={Naoadmin}>
+                    <img src={identidade} alt="icon identidade navbar" />
+                </div>
+            </div>
+        </div>
+        )
+    }else if(logado === 2){
+        return (
+            <div className="container">
+            <div className="container-image">
+                <img src={logo} alt="logo do solf1"/>
+            </div>
+
+            <div className="container-icons">
+                <div className="container-icons__primeiro" onClick={House}>
+                    <img src={house} alt="icon house navbar" />
+                </div>
+
+                <div className="container-icons__segundo" onClick={User}>
+                    <img src={user} alt="icon user navbar" />
+                </div>
+
+                <div className="container-icons__terceiro" onClick={NaoDashboard}>
+                    <img src={galeria} alt="icon galeria navbar" />
+                </div>
+
+                <div className="container-icons__quarto" onClick={Naocartões}>
+                    <img src={cartao} alt="icon cartao navbar" />
+                </div>
+
                 <div className="container-icons__quinto" onClick={admin}>
                     <img src={identidade} alt="icon identidade navbar" />
                 </div>
+            </div>
+
+            <div className="container-user">
+                <div className="container-avatar"><img src={avatar} alt="icon avatar navbar" /></div>
+                <div className="container-exit" onClick={logout}><img src={exit} alt="icon exit navbar" /></div>
             </div>
         </div>
         )
