2173e13435790c222348d717bc9ae19da23d664d
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-03 13:33:37 -0300

    Update signup.jsx

--- Client/src/Pages/Autorizada/Login/SignUp/signup.jsx
+++ Client/src/Pages/Autorizada/Login/SignUp/signup.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React , {useState}from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
 
+import contextAdmin from '../../../../Context/ContextAdmin';
+
 import './signup.css';
 import MenulateralNoUser from '../../../../Components/NavBar No user/menulateralNoUser';
 
 const PageLoginSignUp = () => {
+    const [useLogin, setUseLogin] = useState(1);
+
     const navigate = useNavigate();
 
     const escolheuSignIn = () => {
@@ -15,14 +19,26 @@ const PageLoginSignUp = () => {
     };
 
     const handleClickRegister = (values) => {
-        Axios.post("http://localhost:3001/register", {
+        Axios.post('http://localhost:3001/register', {
             nome: values.nome,
             email: values.email,
             password: values.password,
         }).then((response) => {
             alert(response.data.msg);
             //faço um if do response e se a mensage for boa ele ja atualiza o estado de uma variavel global que sera usada para identificar o usuario
-            console.log(response);
+            if(response.data.admincode === 1){
+                console.log(response);
+                global.variavel = 1;
+                navigate(`/Dashboard`);
+            }else if(response.data.admincode === 2){
+                console.log(response);
+                global.variavel = 2;
+                navigate(`/admin`);
+            }else{
+                console.log(response);
+                global.variavel = 0
+                navigate(`/`);
+            }
         });
     };
 
@@ -51,98 +67,100 @@ const PageLoginSignUp = () => {
     });
 
     return (
-        <div className="signup-container">
-            <MenulateralNoUser></MenulateralNoUser>
-            <div className="signup-container-direita">
-                <div className="signup-container-direita-main">
-                    <h1>Sign up to continue!</h1>
-                    <Formik
-                        initialValues={{}}
-                        onSubmit={handleClickRegister}
-                        validationSchema={validationRegister}
-                    >
-                        <Form className="signup-form">
-                            <div className="signup-form-input">
-                                <Field
-                                    name="nome"
-                                    className="form-field"
-                                    placeholder="Nome Do Usuário"
-                                    autoComplete="off"
-                                ></Field>
-                                <ErrorMessage
-                                    component="span"
-                                    name="nome"
-                                    className="form-error"
-                                ></ErrorMessage>
-                            </div>
+        <contextAdmin.Provider value={[useLogin, setUseLogin]}>
+            <div className="signup-container">
+                <MenulateralNoUser></MenulateralNoUser>
+                <div className="signup-container-direita">
+                    <div className="signup-container-direita-main">
+                        <h1>Sign up to continue!</h1>
+                        <Formik
+                            initialValues={{}}
+                            onSubmit={handleClickRegister}
+                            validationSchema={validationRegister}
+                        >
+                            <Form className="signup-form">
+                                <div className="signup-form-input">
+                                    <Field
+                                        name="nome"
+                                        className="form-field"
+                                        placeholder="Nome Do Usuário"
+                                        autoComplete="off"
+                                    ></Field>
+                                    <ErrorMessage
+                                        component="span"
+                                        name="nome"
+                                        className="form-error"
+                                    ></ErrorMessage>
+                                </div>
 
-                            <div className="signup-form-input">
-                                <Field
-                                    name="email"
-                                    className="form-field"
-                                    placeholder="Email Do Usuário"
-                                    autoComplete="off"
-                                ></Field>
-                                <ErrorMessage
-                                    component="span"
-                                    name="email"
-                                    className="form-error"
-                                ></ErrorMessage>
-                            </div>
+                                <div className="signup-form-input">
+                                    <Field
+                                        name="email"
+                                        className="form-field"
+                                        placeholder="Email Do Usuário"
+                                        autoComplete="off"
+                                    ></Field>
+                                    <ErrorMessage
+                                        component="span"
+                                        name="email"
+                                        className="form-error"
+                                    ></ErrorMessage>
+                                </div>
 
-                            <div className="signup-form-input">
-                                <Field
-                                    name="password"
-                                    className="form-field"
-                                    placeholder="Senha Do Usuário"
-                                    autoComplete="off"
-                                    type="password"
-                                ></Field>
-                                <ErrorMessage
-                                    component="span"
-                                    name="password"
-                                    className="form-error"
-                                ></ErrorMessage>
-                            </div>
+                                <div className="signup-form-input">
+                                    <Field
+                                        name="password"
+                                        className="form-field"
+                                        placeholder="Senha Do Usuário"
+                                        autoComplete="off"
+                                        type="password"
+                                    ></Field>
+                                    <ErrorMessage
+                                        component="span"
+                                        name="password"
+                                        className="form-error"
+                                    ></ErrorMessage>
+                                </div>
 
-                            <div className="signup-form-input">
-                                <Field
-                                    name="confirmPassword"
-                                    className="form-field"
-                                    placeholder="Confirme sua senha"
-                                    autoComplete="off"
-                                    type="password"
-                                ></Field>
-                                <ErrorMessage
-                                    component="span"
-                                    name="confirmPassword"
-                                    className="form-error"
-                                ></ErrorMessage>
-                            </div>
+                                <div className="signup-form-input">
+                                    <Field
+                                        name="confirmPassword"
+                                        className="form-field"
+                                        placeholder="Confirme sua senha"
+                                        autoComplete="off"
+                                        type="password"
+                                    ></Field>
+                                    <ErrorMessage
+                                        component="span"
+                                        name="confirmPassword"
+                                        className="form-error"
+                                    ></ErrorMessage>
+                                </div>
 
-                            <button
-                                className="signup-form-button"
-                                type="submit"
-                            >
-                                SIGN UP
-                            </button>
+                                <button
+                                    className="signup-form-button"
+                                    type="submit"
+                                >
+                                    SIGN UP
+                                </button>
 
-                            <h2 className="signup-container-direita-footer__account">
-                                Você Já tem uma conta ?{' '}
-                                <p onClick={escolheuSignIn}>SIGN IN</p>
-                            </h2>
-                        </Form>
-                    </Formik>
-                </div>
+                                <h2 className="signup-container-direita-footer__account">
+                                    Você Já tem uma conta ?{' '}
+                                    <p onClick={escolheuSignIn}>SIGN IN</p>
+                                </h2>
+                            </Form>
+                        </Formik>
+                    </div>
 
-                <div className="signup-container-direita-footer">
-                    <h2>
-                        Criado por <span>Victor Mesquita</span> &{' '}
-                        <span>Livian</span>
-                    </h2>
+                    <div className="signup-container-direita-footer">
+                        <h2>
+                            Criado por <span>Victor Mesquita</span> &{' '}
+                            <span>Livian</span>
+                        </h2>
+                    </div>
                 </div>
             </div>
-        </div>
+        </contextAdmin.Provider>
     );
 };
 
