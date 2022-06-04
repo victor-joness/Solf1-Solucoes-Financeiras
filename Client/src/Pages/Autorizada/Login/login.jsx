86f3fc576c14246a80cbbdad589f2484adb7d56c
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-04 07:37:32 -0300

    Update login.jsx

--- Client/src/Pages/Autorizada/Login/login.jsx
+++ Client/src/Pages/Autorizada/Login/login.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
-
-import contextAdmin from "../../../Context/ContextAdmin";
+import { useNavigate } from 'react-router-dom';
 
 import "./login.css";
 
