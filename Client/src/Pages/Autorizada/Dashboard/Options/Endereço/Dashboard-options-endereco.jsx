094640442e1d53363dc8c21ed3c7040facce6464
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-09 08:20:52 -0300

    Create Dashboard-options-endereco.jsx

---
+++ Client/src/Pages/Autorizada/Dashboard/Options/Endereço/Dashboard-options-endereco.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Dashboard-options-endereço.css";
+
+import Menulateral from "../../../../../Components/NavBar/menulateral";
+import UserContainer from "../../../../../Components/User/UserContainer";
+
+const PageDashboardOptionsEndereco = () => {
+    
+}
+
+export default PageDashboardOptionsEndereco;
