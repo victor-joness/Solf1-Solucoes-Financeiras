72375f9140960bce54f320e6242f564df61c7da9
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-10 10:59:56 -0300

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -36,7 +36,7 @@ app.post("/register", (req, res) => {
               if (err) {
                 res.send(err);
               }
-              res.send({ msg: "Usuário cadastrado com sucesso", admincode: 1});
+              res.send({ msg: "Usuário cadastrado com sucesso", admincode: 1, email: email, nome: nome});
             }
           );
         });
@@ -60,16 +60,62 @@ app.post("/register", (req, res) => {
             res.send(error);
           }
           if(email == "admin@gmail.com"){
-            res.send({ msg: "admin logado!", admincode: 2});
+            res.send({ msg: "admin logado!", admincode: 2, email: email, result: result});
           }
           if (response) {
-            res.send({ msg: "Usuário logado!", admincode: 1});
+            res.send({ msg: "Usuário logado!", admincode: 1, email: email, result: result});
           } else {
             res.send({ msg: "Senha incorreta!", admincode: 0});
           }
         });
       } else {
         res.send({ msg: "Usuário não registrado!", admincode: 0});
+      }
+    });
+  });
+
+  app.post("/remove", (req, res) => {
+    const email = req.body.email;
+
+    db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+      if(err){
+        res.send(err);
+      }
+      if(result.length > 0){
+        db.query("DELETE FROM usuarios WHERE email = ?", [email], (err, result) => {
+          if(err){
+            res.send(err);
+          }
+          else{
+            res.send({msg: "Conta deletada com Sucesso"});
+          }
+        })
+      }
+    });
+  });
+
+  app.post("/editar", (req, res) => {
+    const email = req.body.email;
+    const nome = req.body.nome;
+    const emailnovo = req.body.emailnovo;
+    const celular1 = req.body.celular1;
+    const password = req.body.senha;
+
+    db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+      if(err){
+        res.send(err);
+      }
+      if(result.length > 0){
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+          db.query("UPDATE usuarios SET nome = ?, email = ? , celular1 = ?, password = ? WHERE email = ?", [nome, emailnovo, celular1, hash, email], (err, result) => {
+            if(err){
+              res.send(err);
+            }
+            else{
+              res.send({msg: "mudaça feita com sucesso", nome: nome, email: emailnovo, celular: celular1, endereco: "Insira um endereco"});
+            }
+          });
+        })
       }
     });
   });
