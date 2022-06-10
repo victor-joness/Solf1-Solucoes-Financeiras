e1062b8de83ef7b2b174d0a05f68fbf05589c8b2
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-10 10:59:39 -0300

    Update Logout.jsx

--- Client/src/Pages/Autorizada/Login/Logout/Logout.jsx
+++ Client/src/Pages/Autorizada/Login/Logout/Logout.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React from 'react';
 
-import "./Logout.css";
+import './Logout.css';
 
-import MenulateralNoUser from "../../../../Components/NavBar No user/menulateralNoUser"
-import logo from "../../../../images/LOGO.png";
-import { useNavigate } from "react-router-dom";
+import MenulateralNoUser from '../../../../Components/NavBar No user/menulateralNoUser';
+import logo from '../../../../images/LOGO.png';
+import { useNavigate } from 'react-router-dom';
+import Axios from 'axios';
 
 const PageAutorizadaLogout = () => {
     const navigate = useNavigate();
 
     const escolheuLOGOUT = () => {
-        window.alert("Usuario Deslogado com sucesso");
+        window.alert('Usuario Deslogado com sucesso');
         global.variavel = 0;
         navigate(`/`);
-    }
+    };
+
+    const escolheuREMOVE = () => {
+        console.log(global.email);
+
+        Axios.post('http://localhost:3001/remove',{
+            email: global.email,
+        }).then((response) => {
+            console.log(response)
+            //faço um if do response e se a mensage for boa ele ja atualiza o estado de uma variavel global que sera usada para identificar o usuario
+            window.alert(response.data.msg);
+
+            navigate(`/`);
+            global.variavel = 0;
+        });
+    };
 
     return (
         <div className="Logout-Container">
@@ -24,18 +40,31 @@ const PageAutorizadaLogout = () => {
                         <img src={logo} alt="logo do solf1" />
                     </div>
                     <div className="Logout-Container-Direita__buttons__options">
-                        <div className="Logout-Container-Direita__buttons__options__signup" onClick={escolheuLOGOUT}>
+                        <div
+                            className="Logout-Container-Direita__buttons__options__signup"
+                            onClick={escolheuLOGOUT}
+                        >
                             <p>LOGOUT</p>
+                        </div>
+
+                        <div
+                            className="Logout-Container-Direita__buttons__options__remove"
+                            onClick={escolheuREMOVE}
+                        >
+                            <p>REMOVER CONTA</p>
                         </div>
                     </div>
                 </div>
 
                 <div className="Logout-Container-Direita__footer">
-                    <h2>Criado por <span>Victor Mesquita</span> & <span>Livian</span></h2>
+                    <h2>
+                        Criado por <span>Victor Mesquita</span> &{' '}
+                        <span>Livian</span>
+                    </h2>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default PageAutorizadaLogout;
