b1e00190a5347f0814d4e99fd398691fe8f12ab8
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-22 20:51:08 -0300

    Create styles.js

---
+++ Client/src/Pages/Autorizada/cartões/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 70px;
+  text-align: center;
+  background: #40B189;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
