c9007ca8b70a182c58420e95ebf8ce7ecea525e9
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-22 20:51:04 -0300

    Create index.js

---
+++ Client/src/Pages/Autorizada/cartões/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import * as C from "./styles";
+
+const Header = () => {
+  const nome = global.nome;
+
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Title>Cadastro de Cartoes do : {nome}</C.Title>
+      </C.Header>
+    </C.Container>
+  );
+};
+
+export default Header;
