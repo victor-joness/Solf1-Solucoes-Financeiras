643a89146675dd63e75e7b361c86471a3094b5e6
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-24 08:26:52 -0300

    Create styles.js

---
+++ Client/src/Pages/Autorizada/Dashboard/Options/Despesa/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: #40B189;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
