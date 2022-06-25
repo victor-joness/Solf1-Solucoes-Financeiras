d930254df95395da6a1cb4d3216c3061377d0be8
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-25 09:12:45 -0300

    Update Dashboard-options-endereco.jsx

--- Client/src/Pages/Autorizada/Dashboard/Options/Endereço/Dashboard-options-endereco.jsx
+++ Client/src/Pages/Autorizada/Dashboard/Options/Endereço/Dashboard-options-endereco.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import "./Dashboard-options-endereço.css";
+import './Dashboard-options-endereço.css';
 
-import Menulateral from "../../../../../Components/NavBar/menulateral";
-import UserContainer from "../../../../../Components/User/UserContainer";
+import Menulateral from '../../../../../Components/NavBar/menulateral';
+import UserContainer from '../../../../../Components/User/UserContainer';
 
 const PageDashboardOptionsEndereco = () => {
-    
-}
+   
+};
 
 export default PageDashboardOptionsEndereco;
