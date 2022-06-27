133646fb033bec64e5141b27d34110a031155885
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-06-26 21:50:45 -0300

    Update Cartões.jsx

--- Client/src/Pages/Autorizada/cartões/Cartões.jsx
+++ Client/src/Pages/Autorizada/cartões/Cartões.jsx
@@ -10,37 +10,11 @@ import './Cartões.css';
 import Menulateral from '../../../Components/NavBar/menulateral';
 
 const PageAutorizadaCartoes = () => {
-    const nome = global.nome;
-    const data = localStorage.getItem('transactions');
+    const data = localStorage.getItem('cartoes');
     const [transactionsList, setTransactionsList] = useState(
         data ? JSON.parse(data) : []
     );
-    const [income, setIncome] = useState(0);
-    const [expense, setExpense] = useState(0);
-    const [total, setTotal] = useState(0);
 
-    useEffect(() => {
-        const amountExpense = transactionsList
-            .filter((item) => item.expense)
-            .map((transaction) => Number(transaction.amount));
-
-        const amountIncome = transactionsList
-            .filter((item) => !item.expense)
-            .map((transaction) => Number(transaction.amount));
-
-        const expense = amountExpense
-            .reduce((acc, cur) => acc + cur, 0)
-            .toFixed(2);
-        const income = amountIncome
-            .reduce((acc, cur) => acc + cur, 0)
-            .toFixed(2);
-
-        const total = Math.abs(income - expense).toFixed(2);
-
-        setIncome(`R$ ${income}`);
-        setExpense(`R$ ${expense}`);
-        setTotal(`${Number(income) < Number(expense) ? '-' : ''}R$ ${total}`);
-    }, [transactionsList]);
 
     const handleAdd = (transaction) => {
         const newArrayTransactions = [...transactionsList, transaction];
@@ -48,8 +22,17 @@ const PageAutorizadaCartoes = () => {
         setTransactionsList(newArrayTransactions);
 
         localStorage.setItem(
-            'transactions',
+            'cartoes',
             JSON.stringify(newArrayTransactions)
+        );
+        const anterior = global.despesas;
+        console.log(anterior)
+
+        const teste = [{id: 200, desc: "teste4", amount: "100", categoria:"teste4", expense: false}];
+
+        localStorage.setItem(
+            'transactions',
+            JSON.stringify(teste)
         );
     };
 
