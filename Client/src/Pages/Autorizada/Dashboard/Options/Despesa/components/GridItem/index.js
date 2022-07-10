323b70bcba9ce9e7fc776d211e68de4f4ccb208a
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-07-10 20:45:33 -0300

    Update index.js

--- Client/src/Pages/Autorizada/Dashboard/Options/Despesa/components/GridItem/index.js
+++ Client/src/Pages/Autorizada/Dashboard/Options/Despesa/components/GridItem/index.js
@@ -4,9 +4,42 @@ import {
   FaRegArrowAltCircleUp,
   FaRegArrowAltCircleDown,
   FaTrash,
+  FaMarker
 } from "react-icons/fa";
 
-const GridItem = ({ item, onDelete }) => {
+const GridItem = ({ item, onDelete, itens, setItens, handleAdd, transactionsList, setTransactionsList}) => {
+
+  const onUpdate = (ID) => {
+    const descricao = (window.prompt("Descricao nova"));
+    const valor = Number(window.prompt("Valor novo nova"));
+    const Categoria = (window.prompt("Categoria nova"));
+    const isExpense = item.expense;
+
+    const generateID = () => Math.round(Math.random() * 1000);
+
+    const transaction = {
+      id: generateID(),
+      desc: descricao,
+      amount: valor,
+      categoria: Categoria,
+      expense: isExpense,
+    };
+
+    
+    const newArrayTransactions = [...transactionsList, transaction];
+    //
+    const newArray = itens.filter((newArrayTransactions) => newArrayTransactions.id !== ID);
+
+    const newArrayTransactions2 = [...newArray, transaction];
+
+    localStorage.setItem('transactions',JSON.stringify(newArrayTransactions2));
+
+    setTransactionsList(newArrayTransactions2);
+
+    //caso n funcionar usar o useNavigate
+  };
+
+
   return (
     <C.Tr>
       <C.Td>{item.desc}</C.Td>
@@ -21,6 +54,10 @@ const GridItem = ({ item, onDelete }) => {
       </C.Td>
       <C.Td alignCenter>
           <FaTrash onClick={() => onDelete(item.id)} />
+      </C.Td>
+      <C.Td alignCenter>
+          <FaMarker onClick={() => onUpdate(item.id)} />
+
       </C.Td>
     </C.Tr>
   );
