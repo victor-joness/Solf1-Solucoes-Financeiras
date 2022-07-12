4a7a0643a4633c357ed1dba9abdc74dc6766cfb5
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-07-12 11:28:27 -0300

    Update index.js

--- Client/src/Pages/Autorizada/cartões/components/Grid/index.js
+++ Client/src/Pages/Autorizada/cartões/components/Grid/index.js
@@ -2,11 +2,29 @@ import React from "react";
 import GridItem from "../GridItem";
 import * as C from "./styles";
 
-const Grid = ({ itens, setItens }) => {
+const Grid = ({ itens, setItens, handleAdd, transactionsList, setTransactionsList }) => {
   const onDelete = (ID) => {
     const newArray = itens.filter((transaction) => transaction.id !== ID);
     setItens(newArray);
     localStorage.setItem("cartoes", JSON.stringify(newArray));
+
+    const data = localStorage.getItem('transactions');
+
+    //const [transactionsList, setTransactionsList] = useState(
+        //data ? JSON.parse(data) : []
+    //);
+
+    console.log(ID);
+
+    const teset = JSON.parse(data);
+
+    const talvez = teset.filter((newArrayTransactions) => newArrayTransactions.id !== ID);
+
+    const newArrayTransactions3 = [...talvez, newArray];
+
+    localStorage.setItem('transactions',JSON.stringify(talvez));
+
+    console.log(newArrayTransactions3);
   };
 
   return (
@@ -24,7 +42,7 @@ const Grid = ({ itens, setItens }) => {
       </C.Thead>
       <C.Tbody>
         {itens?.map((item, index) => (
-          <GridItem key={index} item={item} onDelete={onDelete} />
+          <GridItem key={index} item={item} itens={itens} setItens={setItens} onDelete={onDelete} handleAdd={handleAdd} transactionsList={transactionsList} setTransactionsList={setTransactionsList}/>
         ))}
       </C.Tbody>
     </C.Table>
